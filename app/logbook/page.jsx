3f463febad449e10a993627d01e5c20ccbf8ed9b
irwan <irwandasyafriadi96@gmail.com>
2023-11-22 23:30:25 +0700

    update

--- app/logbook/page.jsx
+++ app/logbook/page.jsx
@@ -21,7 +21,6 @@ const Contact = () => {
             {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
                <TableHeader className='  '>
                     <TableRow className=''>
-                        <TableHead className="">No</TableHead>
                         <TableHead className="">Aktivitas</TableHead>
                         <TableHead className=''>Tanggal / Bulan</TableHead>
                         <TableHead className="text-center">Bukti</TableHead>
@@ -29,7 +28,6 @@ const Contact = () => {
                 </TableHeader>
                 <TableBody className='justify-between'>
                     <TableRow >
-                        <TableCell>1</TableCell>
                         <TableCell className="font-medium">Menentukan tema/judul proposal pjbl dan menyelesaikan pendahuluan</TableCell>
                         <TableCell className='text-center'>12 September 2023</TableCell>
                         <TableCell className="text-right">
@@ -45,7 +43,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>2</TableCell>
                         <TableCell className="font-medium">Pembagian masing-masing tugas dalam beberapa kelompok dan menentukan topik</TableCell>
                         <TableCell className='text-center'>20 September 2023</TableCell>
                         <TableCell className="text-right">
@@ -61,7 +58,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>3</TableCell>
                         <TableCell className="font-medium">Diskusi pembuatan judul </TableCell>
                         <TableCell className='text-center'>24 September 2023</TableCell>
                         <TableCell className="text-right">
@@ -77,7 +73,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>4</TableCell>
                         <TableCell className="font-medium">Meminta persetujuan diskusi judul dengan Ibu Yuli</TableCell>
                         <TableCell className='text-center'>25 September 2023</TableCell>
                         <TableCell className="text-right">
@@ -93,7 +88,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>5</TableCell>
                         <TableCell className="font-medium">Mendiskusikan perihal PjBL dengan Bapak Ryan Hidayatullah</TableCell>
                         <TableCell className='text-center'>25 September 2023</TableCell>
                         <TableCell className="text-right">
@@ -109,7 +103,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>6</TableCell>
                         <TableCell className="font-medium">Membuat planning pengamatan ke lokasi TMII</TableCell>
                         <TableCell className='text-center'>25 September 2023</TableCell>
                         <TableCell className="text-right">
@@ -125,7 +118,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>7</TableCell>
                         <TableCell className="font-medium">Membuat PPT untuk diserahkan kepada dosen mata kuliah Bahasa Indonesia dan di presentasikan</TableCell>
                         <TableCell className='text-center'>26 September 2023</TableCell>
                         <TableCell className="text-right">
@@ -141,7 +133,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>8</TableCell>
                         <TableCell className="font-medium">Melanjutkan pembuatan proposal </TableCell>
                         <TableCell className='text-center'>29 September 2023</TableCell>
                         <TableCell className="text-right">
@@ -157,7 +148,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>9</TableCell>
                         <TableCell className="font-medium">Pelaksanaan penelitian di Taman Mini Indonesia Indah</TableCell>
                         <TableCell className='text-center'>16 Oktober 2023</TableCell>
                         <TableCell className="text-right">
@@ -173,7 +163,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>10</TableCell>
                         <TableCell className="font-medium">Pelaksanaan penelitian di Taman Mini Indonesia Indah</TableCell>
                         <TableCell className='text-center'>18 Oktober 2023</TableCell>
                         <TableCell className="text-right w-[300px] ">
@@ -189,7 +178,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>11</TableCell>
                         <TableCell className="font-medium">Presentasi Hasil Penelitian Project Taman Mini Indonesia Indah</TableCell>
                         <TableCell className='text-center'>23 Oktober 2023</TableCell>
                         <TableCell className="text-right">
@@ -205,7 +193,6 @@ const Contact = () => {
                     </TableRow>
 
                     <TableRow >
-                        <TableCell>12</TableCell>
                         <TableCell className="font-medium">Bimbingan Revisi Laporan dan Artikel</TableCell>
                         <TableCell className='text-center'>6 November 2023</TableCell>
                         <TableCell className="text-right">
